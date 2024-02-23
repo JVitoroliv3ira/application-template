b4dd4367c61f080a3d1c637d8c51317fed4d991c
João Oliveira <jvitoroliveira0711@gmail.com>
2024-02-23 20:49:01 -0300

    feature(backend): Cria Controller responsavel pela autenticacao

---
+++ api/src/main/java/api/controllers/v1/AuthController.java
@@ -0,0 +1,13 @@
+package api.controllers.v1;
+
+import api.services.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RequestMapping(path = "/api/v1/auth")
+@RestController
+public class AuthController {
+    private final UserService userService;
+}
