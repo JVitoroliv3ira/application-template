35a3135ea001d8adfea5777bc0d986caa48a8db4
João Oliveira <jvitoroliveira0711@gmail.com>
2024-02-23 21:57:36 -0300

    feature(backend): Implementa a versao basica do endpoint de cadastro de usuario

--- api/src/main/java/api/controllers/v1/AuthController.java
+++ api/src/main/java/api/controllers/v1/AuthController.java
@@ -1,7 +1,15 @@
 package api.controllers.v1;
 
+import api.dtos.requests.UserRegistrationRequestDTO;
+import api.dtos.responses.ApiResponse;
+import api.models.User;
 import api.services.UserService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,4 +18,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class AuthController {
     private final UserService userService;
+
+    @PostMapping(path = "/register")
+    public ResponseEntity<ApiResponse<User>> register(@Valid @RequestBody UserRegistrationRequestDTO request) {
+        User createdUser = this.userService.register(request.convert());
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(new ApiResponse<>(
+                        createdUser,
+                        "Usuário cadastrado com sucesso.",
+                        null
+                ));
+    }
 }
