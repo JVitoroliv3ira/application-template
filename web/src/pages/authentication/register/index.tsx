941975299c43c2df190486ef5bdae665855a6b38
João Oliveira <jvitoroliveira0711@gmail.com>
2024-03-02 16:25:00 -0300

    feature(frontend): Utiliza o HeaderComponent na tela de cadastro de usuario

--- web/src/pages/authentication/register/index.tsx
+++ web/src/pages/authentication/register/index.tsx
@@ -1,19 +1,16 @@
+import HeaderComponent from "../../../components/header";
+
 const RegisterPage = () => {
   return (
     <div className="flex flex-col max-w-full w-full h-screen bg-slate-300">
-      <header className="flex items-center justify-between w-full p-5 bg-slate-100">
-        <a href="/app/main" className="text-lg">
-          Application Template
+      <HeaderComponent>
+        <a className="text-sm font-light" href="/app/auth/register">
+          Cadastre-se
         </a>
-        <div className="space-x-4">
-          <a className="text-sm font-light" href="/app/auth/register">
-            Cadastre-se
-          </a>
-          <a className="text-sm font-light" href="/app/auth/login">
-            Entrar
-          </a>
-        </div>
-      </header>
+        <a className="text-sm font-light" href="/app/auth/login">
+          Entrar
+        </a>
+      </HeaderComponent>
     </div>
   );
 }
