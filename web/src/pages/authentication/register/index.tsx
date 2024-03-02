319b8993124fee26715e1e85bcb7ca7350b4184f
João Oliveira <jvitoroliveira0711@gmail.com>
2024-03-02 12:19:24 -0300

    feature(frontend): Implementa header da pagina de cadastro de usuarios

--- web/src/pages/authentication/register/index.tsx
+++ web/src/pages/authentication/register/index.tsx
@@ -1,7 +1,19 @@
 const RegisterPage = () => {
   return (
-    <div className="max-w-full w-full h-screen bg-slate-300">
-      
+    <div className="flex flex-col max-w-full w-full h-screen bg-slate-300">
+      <header className="flex items-center justify-between w-full p-5 bg-slate-100">
+        <a href="/app/main" className="text-lg">
+          Application Template
+        </a>
+        <div className="space-x-4">
+          <a className="text-sm font-light" href="/app/auth/register">
+            Cadastre-se
+          </a>
+          <a className="text-sm font-light" href="/app/auth/login">
+            Entrar
+          </a>
+        </div>
+      </header>
     </div>
   );
 }
