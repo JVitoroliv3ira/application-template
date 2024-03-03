f9f6d7385bd72b0e99fb060395202705dda1650e
João Oliveira <jvitoroliveira0711@gmail.com>
2024-03-02 22:00:23 -0300

    feature(frontend): Utiliza o layout de usuario nao autenticado na tela de cadastro de usuario

--- web/src/pages/authentication/register/index.tsx
+++ web/src/pages/authentication/register/index.tsx
@@ -4,7 +4,7 @@ import { useNavigate } from "react-router-dom";
 import { useState } from "react";
 import { z } from "zod";
 
-import HeaderComponent from "../../../components/header";
+import UnauthenticatedLayout from "../../../components/templates/unauthenticated-layout";
 
 import UserRegistrationRequestDTO, { validationSchema } from "../../../core/dtos/requests/user-registration-request.dto";
 import ApiResponseDTO from "../../../core/dtos/responses/api-response.dto";
@@ -64,81 +64,71 @@ const RegisterPage = () => {
   };
 
   return (
-    <div className="flex flex-col max-w-full w-full h-screen bg-slate-300">
-      <HeaderComponent>
-        <a className="text-sm font-light" href="/app/auth/register">
-          Cadastre-se
-        </a>
-        <a className="text-sm font-light" href="/app/auth/login">
-          Entrar
-        </a>
-      </HeaderComponent>
-      <main className="flex flex-col justify-center items-center w-full h-screen p-5">
-        <Formik initialValues={initialValues} onSubmit={handleSubmit}>
-          <Form className="bg-slate-100 max-w-sm w-full shadow-md rounded px-8 pt-6 pb-8 mb-4">
-            <div className="mb-4">
-              <label
-                className="block text-gray-700 text-sm font-bold mb-2"
-                htmlFor="name"
-              >
-                Nome
-              </label>
-              <Field
-                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                id="name"
-                name="name"
-                type="text"
-                placeholder="Informe o seu nome"
-              />
-            </div>
-            <div className="mb-4">
-              <label
-                className="block text-gray-700 text-sm font-bold mb-2"
-                htmlFor="email"
-              >
-                Email
-              </label>
-              <Field
-                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                id="email"
-                name="email"
-                type="email"
-                placeholder="Informe o seu email"
-              />
-            </div>
-            <div className="mb-4">
-              <label
-                className="block text-gray-700 text-sm font-bold mb-2"
-                htmlFor="email"
-              >
-                Senha
-              </label>
-              <Field
-                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                id="password"
-                name="password"
-                type="password"
-                placeholder="Informe a sua senha"
-              />
-            </div>
-            <div className="mb-4">
-              <a className="text-sm font-light" href="/app/auth/login">
-                Já possui uma conta?
-              </a>
-            </div>
-            <div className="w-full mb-4">
-              <button
-                className={`flex justify-center items-center w-full rounded text-white bg-slate-950 px-3 py-2 ${loading ? 'opacity-50 cursor-not-allowed' : ''}`}
-                disabled={loading}
-                type="submit"
-              >
-                {!loading ? <span>Cadastre-se</span> : <img src={loadingIcon} alt="Carregando" />}
-              </button>
-            </div>
-          </Form>
-        </Formik>
-      </main>
-    </div>
+    <UnauthenticatedLayout>
+      <Formik initialValues={initialValues} onSubmit={handleSubmit}>
+        <Form className="bg-slate-100 max-w-sm w-full shadow-md rounded px-8 pt-6 pb-8 mb-4">
+          <div className="mb-4">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlFor="name"
+            >
+              Nome
+            </label>
+            <Field
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="name"
+              name="name"
+              type="text"
+              placeholder="Informe o seu nome"
+            />
+          </div>
+          <div className="mb-4">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlFor="email"
+            >
+              Email
+            </label>
+            <Field
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="email"
+              name="email"
+              type="email"
+              placeholder="Informe o seu email"
+            />
+          </div>
+          <div className="mb-4">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlFor="email"
+            >
+              Senha
+            </label>
+            <Field
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="password"
+              name="password"
+              type="password"
+              placeholder="Informe a sua senha"
+            />
+          </div>
+          <div className="mb-4">
+            <a className="text-sm font-light" href="/app/auth/login">
+              Já possui uma conta?
+            </a>
+          </div>
+          <div className="w-full mb-4">
+            <button
+              className={`flex justify-center items-center w-full rounded text-white bg-slate-950 px-3 py-2 ${loading ? 'opacity-50 cursor-not-allowed' : ''}`}
+              disabled={loading}
+              type="submit"
+            >
+              {!loading ? <span>Cadastre-se</span> : <img src={loadingIcon} alt="Carregando" />}
+            </button>
+          </div>
+        </Form>
+      </Formik>
+    </UnauthenticatedLayout>
   );
 }
 
