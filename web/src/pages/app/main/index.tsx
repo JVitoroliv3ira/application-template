0720bb38b59cb8141bc7751700fd0636235c675f
João Oliveira <jvitoroliveira0711@gmail.com>
2024-03-03 10:11:17 -0300

    feature(frontend): Cria pagina principal da aplicacao

---
+++ web/src/pages/app/main/index.tsx
@@ -0,0 +1,11 @@
+import AuthenticatedLayout from "../../../components/templates/authenticated-layout";
+
+const MainPage = () => {
+  return (
+    <AuthenticatedLayout>
+
+    </AuthenticatedLayout>
+  );
+}
+
+export default MainPage;
