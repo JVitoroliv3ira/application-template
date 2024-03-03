ddd4fa3acd13f019cd6e4576691d57e250f851b6
João Oliveira <jvitoroliveira0711@gmail.com>
2024-03-03 09:49:47 -0300

    feature(frontend): Adiciona funcoes responsaveis por manipular token e email

--- web/src/core/services/user-authentication.service.ts
+++ web/src/core/services/user-authentication.service.ts
@@ -1,7 +1,11 @@
 import UserAuthenticationRequestDTO from "../dtos/requests/user-authentication-request.dto";
 import ApiResponseDTO from "../dtos/responses/api-response.dto";
 import AuthenticatedUserResponseDTO from "../dtos/responses/authenticated-user-response.dto";
+import { get, insert, remove } from "../utils/local-storage-util";
 import { post } from "./base-http.service";
+
+const TOKEN_KEY = "AUTHENTICATION_USER_TOKEN";
+const EMAIL_KEY = "AUTHENTICATION_USER_EMAIL";
 
 const userAuthenticationService = async (content: UserAuthenticationRequestDTO): Promise<ApiResponseDTO<AuthenticatedUserResponseDTO>> => {
   return await post<UserAuthenticationRequestDTO, ApiResponseDTO<AuthenticatedUserResponseDTO>>(
@@ -10,4 +14,12 @@ const userAuthenticationService = async (content: UserAuthenticationRequestDTO):
   );
 }
 
+const saveToken = (token: string): void => insert(TOKEN_KEY, token);
+const saveEmail = (email: string): void => insert(EMAIL_KEY, email);
+const getToken = (): string | null => get(TOKEN_KEY);
+const getEmail = (): string | null => get(EMAIL_KEY);
+const removeToken = (): void => remove(TOKEN_KEY);
+const removeEmail = (): void => remove(EMAIL_KEY);
+
 export default userAuthenticationService;
+export { saveToken, saveEmail, getToken, getEmail, removeToken, removeEmail };
