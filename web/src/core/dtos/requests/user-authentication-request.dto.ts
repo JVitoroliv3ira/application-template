38dfd9a8ae0bc647f396a48d7a5447733299f77e
João Oliveira <jvitoroliveira0711@gmail.com>
2024-03-03 09:49:14 -0300

    feature(frontend): Adiciona validacao para a request de autenticacao

--- web/src/core/dtos/requests/user-authentication-request.dto.ts
+++ web/src/core/dtos/requests/user-authentication-request.dto.ts
@@ -1,6 +1,18 @@
+import { z } from "zod";
+
 interface UserAuthenticationRequestDTO {
   email: string;
   password: string;
 }
 
+const validationSchema = z.object({
+  email: z.string()
+    .email({ message: "O email informado é inválido." })
+    .min(5, { message: "O email deve conter no mínimo 5 caracteres." })
+    .max(80, { message: "O email deve conter no máximo 80 caracteres." }),
+  password: z.string()
+    .min(6, { message: "A senha deve conter no mínimo 6 caracteres." })
+});
+
 export default UserAuthenticationRequestDTO;
+export { validationSchema };
