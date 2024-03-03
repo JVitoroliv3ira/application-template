62c058eea2c849ad93f9cdc2658e8170b2d11437
João Oliveira <jvitoroliveira0711@gmail.com>
2024-03-03 10:32:32 -0300

    feature(frontend): Utiliza guard de usuario autenticado na rota /app/main

--- web/src/routes/index.tsx
+++ web/src/routes/index.tsx
@@ -3,6 +3,7 @@ import RegisterPage from '../pages/authentication/register';
 import { ToastContainer } from 'react-toastify';
 import LoginPage from '../pages/authentication/login';
 import MainPage from '../pages/app/main';
+import AuthGuard from './guards/auth-guard';
 
 const AppRouter = () => {
   return (
@@ -11,7 +12,11 @@ const AppRouter = () => {
       <Routes>
         <Route path='/app/auth/register' element={<RegisterPage />} />
         <Route path='/app/auth/login' element={<LoginPage />} />
-        <Route path='/app/main' element={<MainPage />} />
+        <Route path="/app/main" element={
+          <AuthGuard>
+            <MainPage />
+          </AuthGuard>
+        } />
       </Routes>
     </Router>
   );
