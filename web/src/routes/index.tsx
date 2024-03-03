a1f7440648e18ade776e3b7e8645fc32d9b92736
João Oliveira <jvitoroliveira0711@gmail.com>
2024-03-03 10:11:44 -0300

    feature(frontend): Adiciona a nova pagina no roteador da aplicacao

--- web/src/routes/index.tsx
+++ web/src/routes/index.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import RegisterPage from '../pages/authentication/register';
 import { ToastContainer } from 'react-toastify';
 import LoginPage from '../pages/authentication/login';
+import MainPage from '../pages/app/main';
 
 const AppRouter = () => {
   return (
@@ -10,6 +11,7 @@ const AppRouter = () => {
       <Routes>
         <Route path='/app/auth/register' element={<RegisterPage />} />
         <Route path='/app/auth/login' element={<LoginPage />} />
+        <Route path='/app/main' element={<MainPage />} />
       </Routes>
     </Router>
   );
